42bf1498da7f93919d0f7669ffb0e272c3717662
mandalmasum97 <mandalmasum97@gmail.com>
2020-05-17 17:08:42 +0530

    Handled File path

--- src/FileReader.java
+++ src/FileReader.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.lang.*;
 public class FileReader {
     String readfile()throws Exception{
-        File file = new File("C:\\Users\\acer\\eclipse-workspace\\Project\\src\\Paragraph.txt");
+        File file = new File("src\\Paragraph.txt");
        // BufferedReader br = new BufferedReader(new java.io.FileReader(file));
         
         Reader reader =new InputStreamReader(new FileInputStream(file),"UTF-8");
